10d2588f1c405770da40d68ab707e2cf21adcf32
Thamsanqa <thnyezijhb024@student.wethinkcode.co.za>
2025-01-04 12:41:53 +0200

    created two variables and used a while loop

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,8 +1,14 @@
 const repeatString = function(string, num) {
- 
-
+    let word = string;
+    let i = 1;
+    while (i < num) {
+        console.log(word);
+        i++;
+    }
+    return word;
 };
 console.log(repeatString("hey", 3));
+
 
 // Do not edit below this line
 module.exports = repeatString;
