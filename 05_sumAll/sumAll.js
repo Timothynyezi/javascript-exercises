872ac5fca90f792d16f3fc75aa3470f607787990
Thamsanqa <thnyezijhb024@student.wethinkcode.co.za>
2025-01-06 19:08:47 +0200

    tried two lines of code

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,11 @@
 const sumAll = function(num1, num2) {
     if ( (num1 < 0 || num2 < 0) ) return "ERROR";
-    
+    else if (typeof num1 !== "number" || !Number.isInteger(num1)) {
+        throw new Error ("ERROR");
+    }
+    else if (typeof num2 !== "number" || !Number.isInteger(num2)) {
+        throw new Error("ERROR");
+    }
     let finalSum = 0;
     for (let i = num1; (i <= num1 && i >= num2) || (i >= num1 && i <= num2); i++) {
         finalSum += i;
