af4c5e4520724c253b9b46bc3739de8b795f9350
Thamsanqa <thnyezijhb024@student.wethinkcode.co.za>
2025-01-06 17:31:25 +0200

    wrote code for the body of the for loop

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -2,7 +2,7 @@ const sumAll = function(num1, num2) {
     if ( num1 < 0 || num2 < 0) return "ERROR";
     let finalSum = 0;
     for (let i = num1; i >= num1 && i <= num2; i++) {
-        
+        finalSum += i;
 
     };
     
