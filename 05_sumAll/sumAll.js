906badeb2e4d52cc3a968f7d4298d5005198fd2d
Thamsanqa <thnyezijhb024@student.wethinkcode.co.za>
2025-01-06 18:55:14 +0200

    removed the commented line of code

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,6 @@
 const sumAll = function(num1, num2) {
     if ( (num1 < 0 || num2 < 0) ) return "ERROR";
-    //if (!num1.isInteger(num1) || !num2.isInteger(num2)) return "ERROR";
+    
     let finalSum = 0;
     for (let i = num1; (i <= num1 && i >= num2) || (i >= num1 && i <= num2); i++) {
         finalSum += i;
