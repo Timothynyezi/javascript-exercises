c1b47b2758d720f4308cda83feacb5a5cd6ba946
Thamsanqa <thnyezijhb024@student.wethinkcode.co.za>
2025-01-06 20:50:46 +0200

    wrote functionality code

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(c) {
+  return Math.round(c - 32) * 5 / 9;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(f) {
+  return Math.round(f * 9 / 5 + 32);
 };
 
 // Do not edit below this line
