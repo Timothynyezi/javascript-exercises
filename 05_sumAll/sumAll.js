13474bff1952dfa5ac06b6bc3fa58c726e7fbd6c
Thamsanqa <thnyezijhb024@student.wethinkcode.co.za>
2025-01-06 17:29:55 +0200

    created a for loop

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,10 @@
 const sumAll = function(num1, num2) {
     if ( num1 < 0 || num2 < 0) return "ERROR";
+    let finalSum = 0;
+    for (let i = num1; i >= num1 && i <= num2; i++) {
+        
+
+    };
     
 };
 
