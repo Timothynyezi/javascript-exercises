f9783aab835f686883aeb8334934dfea3f305e34
Thamsanqa <thnyezijhb024@student.wethinkcode.co.za>
2025-01-06 17:42:03 +0200

    added another condition statement inside of the for loop

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,7 +1,7 @@
 const sumAll = function(num1, num2) {
     if ( num1 < 0 || num2 < 0) return "ERROR";
     let finalSum = 0;
-    for (let i = num1; i >= num1 && i <= num2; i++) {
+    for (let i = num1; (i >= num1 && i <= num2) || (i <= num1 && i >= num2); i++) {
         finalSum += i;
 
     };
