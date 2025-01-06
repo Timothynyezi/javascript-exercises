0a482929af70e60aa9ad24434e82dec65134ecbe
Thamsanqa <thnyezijhb024@student.wethinkcode.co.za>
2025-01-06 19:53:44 +0200

    activated all test cases, and changed some variable names

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,10 +1,10 @@
 const sumAll = function(num1, num2) {
     if (!Number.isInteger(num1) || !Number.isInteger(num2)) return "ERROR";
-    if ( (num1 < 0 || num2 < 0) ) return "ERROR";
+    if (num1 < 0 || num2 < 0) return "ERROR";
     if (num1 > num2) {
     const temp = num1;
-    min = num2;
-    max = temp;
+    num1 = num2;
+    num2 = temp;
   }
     let finalSum = 0;
     for (let i = num1; i <= num2; i++) {
