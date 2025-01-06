e2f194276ba27c18baf4db3c61e75b0c50c90fb9
Thamsanqa <thnyezijhb024@student.wethinkcode.co.za>
2025-01-06 19:50:15 +0200

    changed a few lines of code

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,13 +1,13 @@
 const sumAll = function(num1, num2) {
+    if (!Number.isInteger(num1) || !Number.isInteger(num2)) return "ERROR";
     if ( (num1 < 0 || num2 < 0) ) return "ERROR";
-    else if (typeof num1 !== "number" || !Number.isInteger(num1)) {
-        throw new Error ("ERROR");
-    }
-    else if (typeof num2 !== "number" || !Number.isInteger(num2)) {
-        throw new Error("ERROR");
-    }
+    if (num1 > num2) {
+    const temp = num1;
+    min = num2;
+    max = temp;
+  }
     let finalSum = 0;
-    for (let i = num1; (i <= num1 && i >= num2) || (i >= num1 && i <= num2); i++) {
+    for (let i = num1; i <= num2; i++) {
         finalSum += i;
 
     };
