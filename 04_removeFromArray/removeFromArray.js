4ce1e01b69cfcaa37dd243f6293cc8f26848b331
Thamsanqa <thnyezijhb024@student.wethinkcode.co.za>
2025-01-06 14:19:23 +0200

    Wrote functionality code using a function that takes two arguements and to remove a object inside an array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,12 @@
-const removeFromArray = function(arr, num) {
-
+const removeFromArray = function(Array, num) {
+   Array.forEach((item, index) => {
+    if (item === num) {
+        Array.splice(index, 1);
+    }
+   });
+   return Array;
 };
-
+//let Array = [1, 2, 3, 4];
+//removeFromArray(Array, 3);
 // Do not edit below this line
 module.exports = removeFromArray;
